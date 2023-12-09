e0cb6f5d2a30759a192f5ff59b8f606c6c93af98
Lukas <131675716+Lukazzer@users.noreply.github.com>
2023-12-09 18:05:23 +0100

    Create index.php
    test

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Willkommen</title>
+</head>
+<body>
+    <header>
+        <h1>Willkommen</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="index.php">Startseite</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <p>Dies ist eine einfache PHP-Website. Willkommen!</p>
+    </main>
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Meine Text-Website. Alle Rechte vorbehalten.</p>
+    </footer>
+</body>
+</html>
