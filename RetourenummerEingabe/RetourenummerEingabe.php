fbfad649c47a27303f9ed121b631aa0546292e46
Lukas <lukasliss@web.de>
2023-12-20 11:18:48 +0100

    Anpassung an Mockups

---
+++ RetourenummerEingabe/RetourenummerEingabe.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rückgabe der Bestellung</title>
+    <link rel="stylesheet" href="RetourenummerEingabe.css">
+</head>
+
+<body>
+    <div class="container_Info">
+        Retoure einsehen
+    </div>
+    <div class="container">
+        <input type="text" id="returnNumber" placeholder="Retourenummer eingeben...">
+        <button onclick="getRetoureInfo()">OK</button>
+    </div>
+</body>
+
+</html>
