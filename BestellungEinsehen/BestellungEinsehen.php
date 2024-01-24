74767997664fbdd06bc721544b6c424f3e24477f
SokratesMir <daniel-gudi@web.de>
2024-01-24 17:40:28 +0100

    Update BestellungEinsehen based on database

--- BestellungEinsehen/BestellungEinsehen.php
+++ BestellungEinsehen/BestellungEinsehen.php
@@ -16,6 +16,26 @@
   if (isset($_GET['redirected']) && $_GET['redirected'] == 'true' && isset($_GET['orderNumber'])) {
     // Die Seite wurde vom Skript weitergeleitet
     $orderNumber = $_GET['orderNumber'];
+
+    $db_handle = pg_connect("host=postgresql-database-server.postgres.database.azure.com dbname=reklamation_db user=coolman password=6L_.?6=8T8a~]cy");
+    $orderNumber = $_GET['orderNumber']; // Die Bestellnummer aus der URL
+  
+    // Vorbereiten und Ausführen der SQL-Abfrage
+    $query = "SELECT p.id, p.name, p.img_path, cp.quantity FROM product p INNER JOIN customer_product cp ON p.id = cp.product_id WHERE cp.id = $1";
+    $result = pg_prepare($db_handle, "my_query", $query);
+    $result = pg_execute($db_handle, "my_query", array($orderNumber));
+
+    if ($result) {
+      $row = pg_fetch_assoc($result);
+    } else {
+      // Weiterleitung zurück zur ursprünglichen Seite
+      pg_close($db_handle);
+      $script = "<script>window.location.href = 'https://reklamationsmaster.azurewebsites.net/index.php';</script>";
+      echo $script;
+      exit;
+    }
+
+    pg_close($db_handle);
   } else {
     // Weiterleitung zurück zur ursprünglichen Seite
     $script = "<script>window.location.href = 'https://reklamationsmaster.azurewebsites.net/index.php';</script>";
@@ -32,28 +52,31 @@
   ?>
   <div class="container_body">
     <div class="container_header">
-      Bestellung: 1919191
+      Bestellung:
+      <?php echo htmlspecialchars($orderNumber); ?>
     </div>
     <div class="container_previewImages">
       <div class="wrapper">
         <div class="imageLabel">
-          <label for="topImageLabel">Aspirin Protect 100mg</label>
+          <label for="topImageLabel">
+            <?php echo htmlspecialchars($row['name']); ?>
+          </label>
         </div>
         <div class="image">
-          <img src="<?php echo $imagePath; ?>" alt="image" class="previewImage">
+          <img src="<?php echo htmlspecialchars($row['img_path']); ?>" alt="image" class="previewImage">
           <div class="refundCounter">
-            <label class="Label_left">3x</label>
+            <label class="Label_left">
+              <?php echo htmlspecialchars($row['quantity']) . 'x'; ?>
+            </label>
             <select id="refundCount" name="refundCount" required>
-              <option value="" enabled selected>0x</option>
-              <option value="defective">1x</option>
-              <option value="wrongItem">2x</option>
-              <option value="changedMind">3x</option>
-
-
+              <?php
+              for ($i = 0; $i <= $row['quantity']; $i++) {
+                echo "<option value='$i'>" . $i . "x</option>";
+              }
+              ?>
             </select>
           </div>
         </div>
-
         <div class="imageLabel">
           <label for="topImageLabel">12322232</label>
         </div>
@@ -68,7 +91,6 @@
         <option value="wrongItem">Falsches Produkt erhalten</option>
         <option value="changedMind">Meinung geändert</option>
         <option value="changedMind">Sonstige </option>
-
       </select>
     </div>
 
@@ -76,15 +98,8 @@
       <label for="refundReason">Beschreibung der Probleme:</label>
       <textarea id="text" name="text" cols="35" rows="4"></textarea>
       <button id="submit">Rücksendung einreichen</button>
-
     </div>
-
-
   </div>
-
-
-
-
 </body>
 
 </html>
