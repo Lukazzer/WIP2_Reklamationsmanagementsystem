6248b89fc9ed4fed70c8fd6531284f45bace7dc2
Lukas <lukasliss@web.de>
2024-01-26 16:14:15 +0100

    get prodct info test

--- Datenbankverbindung.php
+++ Datenbankverbindung.php
@@ -37,5 +37,62 @@
         pg_close($conn);
     }
 
+    function getArrayProductInfo()
+    {
+        $conn = connectdb();
+
+        $query = "
+    SELECT
+        c.id AS Retourenummer,
+        cp.quantity AS Menge,
+        cr.description AS Grund,
+        e.name AS 'Zugewiesener Mitarbeiter',
+        p.img_path AS Bildpfad
+    FROM
+        complaint c
+    JOIN
+        complaint_customer_product ccp ON c.id = ccp.complaint_id
+    JOIN
+        customer_product cp ON ccp.customer_product_id = cp.id
+    JOIN
+        complaint_reason cr ON c.reason_id = cr.id
+    LEFT JOIN
+        employee e ON c.employee_id = e.id
+    LEFT JOIN
+        product p ON cp.product_id = p.id;
+";
+
+        $result = pg_query($conn, $query);
+
+
+        if (!$result) {
+            die("Abfrage fehlgeschlagen");
+        }
+
+
+        $resultArray = array();
+        while ($row = pg_fetch_assoc($result)) {
+            $resultArray[] = $row;
+        }
+
+
+        print_r($resultArray);
+
+        echo "<pre>" . print_r($resultArray, true) . "</pre>";
+        
+        pg_close($conn);
+    }
+
 
     ?>
+
+ <!-- 
+    
+Retourenummer 
+Menge
+Grund
+Zugewiesener Mitarbeiter
+Bildpfad
+
+
+-->
